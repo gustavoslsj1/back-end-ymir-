8127acb279db02c22a6f6f3286aa91022fcc875a
Gustavo Lima Sales <102685940+gustavoslsj1@users.noreply.github.com>
2025-06-23 01:44:31 -0300

    implementando o guard.auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -21,7 +21,7 @@ import { JwtModule } from '@nestjs/jwt';
     },
     AuthService,
   ],
-  exports: [HashingServiceProtocol],
+  exports: [HashingServiceProtocol, JwtModule, ConfigModule],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,10 +7,15 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreatUserDto } from './dto/creat-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthTokenGuard } from 'src/auth/guard/auth-token.guard';
+import { REQUEST_TOKEN_PAYLOAD_NAME } from 'src/auth/common/auth.constants';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -24,11 +29,15 @@ export class UsersController {
   createUser(@Body() createUserDto: CreatUserDto) {
     return this.userService.create(createUserDto);
   }
+  @UseGuards(AuthTokenGuard)
   @Patch(':id')
   updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request,
   ) {
+    console.log(req[REQUEST_TOKEN_PAYLOAD_NAME]);
+
     return this.userService.update(id, updateUserDto);
   }
 
